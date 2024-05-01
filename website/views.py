379e4d8d8842a0715c2f1be4b28c15d3ff93f542
Rastin Rostami zadeh <165579071+Rma87@users.noreply.github.com>
2024-05-02 00:10:22 +0330

    created render html file
    added templates folder and html file to website app and render

--- website/views.py
+++ website/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 
 def home_page(request):
-    return HttpResponse('<h1>this is a Home page<h1>')
+    return render(request, 'home.html')
 
 def about_page(request):
-    return HttpResponse('<h1>this is a About page<h1>')
+    return render(request, 'about.html')
