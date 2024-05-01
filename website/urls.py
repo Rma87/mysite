6f500fba376965c14d986231bfa99e1d14ba262e
Rastin Rostami zadeh <165579071+Rma87@users.noreply.github.com>
2024-05-01 23:36:00 +0330

    Created first app
    Create first app named website with two views

---
+++ website/urls.py
@@ -0,0 +1,8 @@
+
+from django.urls import path
+from website.views import home_page, about_page
+
+urlpatterns = [
+    path('home/',home_page),
+    path('about/',about_page)
+]
---
+++ website/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home_page(request):
+    return HttpResponse('<h1>this is a Home page<h1>')
+
+def about_page(request):
+    return HttpResponse('this is a About page')
