dbc4884180bda3bef9ab2f32f2625793a8c2ec7e
Rastin Rostami zadeh <165579071+Rma87@users.noreply.github.com>
2024-05-03 19:23:53 +0330

    change name
    change name home.html

--- website/views.py
+++ website/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 
 def home_page(request):
-    return render(request, 'website/home.html')
+    return render(request, 'website/index.html')
 
 def about_page(request):
     return render(request, 'website/about.html')
