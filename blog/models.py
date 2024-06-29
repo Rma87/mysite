66fe27b33697794efdcc49d7d6ccb70e612e1f1f
Rastin Rostami zadeh <rrastin633@gmail.com>
2024-06-29 21:19:53 +0330

    Data Base
    Created my data base Post and contact

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    # image
+    # author
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    # tag 
+    # category
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateField(null=True)
+    published_time = models.TimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_date']
+    
+    def __str__(self) -> str:
+        return " {} - {} ".format(self.titele, self.id)
--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from website.models import Contact
 
-# Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    list_display = ('name', 'subject', 'email', 'created_date')
+    empty_value_display = '-empty-'
+    list_filter = ('created_date', 'email')
+    search_fields = ['name', 'email', 'email']
+    
+admin.site.register(Contact, ContactAdmin)
